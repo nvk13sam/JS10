4666b123643d966ea1beb088e1399ffe8b9ad4d8
nvk13sam <nvk13@yandex.ru>
2023-01-26 23:40:55 +0500

    add homework10

---
+++ data.js
@@ -0,0 +1,26 @@
+const apiInfo = {
+    "url": "https: //api.nasa.gov/planetary/apod?api_key=of4UQ3vImH7wGMWCWOzcbfhCJgAWMq8Xv4EqHMJa"
+}
+const dataInfo = `[
+{
+"id": "1",
+"url": "https://catdogcity.ru/wp-content/uploads/2020/08/dog.jpg",
+"name": "Marusya",
+"age": 12,
+"cont_text": "описание объекта1"
+},
+{
+"id": "2",
+"url": "https://rabotatam.ru/uploads/monthly_2017_04/large.58f1bdeda1c5c_.jpg.91c33c120ad86a4dbf2c50dd44d00890.jpg",
+"name": "Svetik",
+"age": 5,
+"cont_text": "описание объекта2"
+},
+{
+"id": "3",
+"url": "https://chudo-prirody.com/uploads/posts/2021-08/1628905045_202-p-skachat-foto-milikh-kotikov-216.jpg",
+"name": "king",
+"age": 8,
+"cont_text": "описание объекта3"
+}
+]`
